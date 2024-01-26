2f8f241dd0592d6dd9381489fcb53f8dea9201c6
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2024-01-26 09:39:50 +0530

    Fix API endpoint in getScholarData function

--- app/IpLogger.tsx
+++ app/IpLogger.tsx
@@ -9,33 +9,19 @@ export function IpLogger() {
   const [location, setLocation] = useState("Earth");
   const path = usePathname() || "undefined";
   useEffect(() => {
-    fetch("https://ip.kapil.app", { redirect: "follow" })
+    fetch("https://rpjrwkievzjevdiqhvtv.supabase.co/functions/v1/ip")
       .then((res) => res.json())
       .catch((err) => {
         console.log(err);
         setIp("0.0.0.0");
       })
-      .then((ip: any) => {
-        if (ip !== undefined) {
-          setIp(ip.ip);
+      .then((data: any) => {
+        if (data !== undefined) {
+          setIp(data.ip);
+          setLocation(`${data.city}, ${data.country_name}`);
         }
       });
   }, []);
-  useEffect(() => {
-    if (ip !== "0.0.0.0") {
-      fetch(`https://ipapi.co/${ip}/json`)
-        .then((res) => res.json())
-        .catch((err) => {
-          console.log(err);
-          setLocation("Earth");
-        })
-        .then((data) => {
-          if (data !== undefined) {
-            setLocation(`${data.city}, ${data.country_name}`);
-          }
-        });
-    }
-  }, [ip]);
   useEffect(() => {
     if (
       ip !== "0.0.0.0" &&
@@ -48,6 +34,7 @@ export function IpLogger() {
       path !== "undefined"
     ) {
       saveVisitorLog({ path, ip, location });
+      console.log(path, ip, location);
     }
   }, [ip, location, path]);
   return <span></span>;
