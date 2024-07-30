5b1f88de64011ba8dda4f4f22d14e0c0d967e9cd
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2024-07-30 15:37:50 +0530

    session-storage-for-ip-location

--- app/IpLogger.tsx
+++ app/IpLogger.tsx
@@ -3,33 +3,44 @@
 import { useState, useEffect } from "react";
 import { saveVisitorLog } from "app/db/actions";
 import { usePathname } from "next/navigation";
+import useStorage from 'app/components/helpers/useStorage';
 
 export function IpLogger() {
   const whiteListIp = ["0.0.0.0"];
+  const { getItem, setItem } = useStorage();
+  const sessionIP = getItem('sessionIP') || '';
+  const sessionLocation = getItem('sessionLocation') || '';
   const [ip, setIp] = useState("0.0.0.0");
   const [location, setLocation] = useState("Earth");
   const path = usePathname() || "undefined";
-  let endpoint;
-  var rand_boolean = Math.random() < 0.5;
+  // let endpoint;
+  // var rand_boolean = Math.random() < 0.5;
   // if (rand_boolean) {
   //   endpoint = `https://api.kapil.app/api/ip`
   // } else {
   //   endpoint = `https://api-kapil.netlify.app/api/ip`
   // }
   useEffect(() => {
-    fetch(`https://api2.kapil.app/api/ip`)
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log(err);
-        setIp("0.0.0.0");
-      })
-      .then((data: any) => {
-        if (data !== undefined) {
-          setIp(data.ip);
-          setLocation(`${data.city}, ${data.country_name}`);
-        }
-      });
-  }, []);
+    if (!sessionIP || !sessionLocation) {
+      fetch(`https://api2.kapil.app/api/ip`)
+        .then((res) => res.json())
+        .catch((err) => {
+          console.log(err);
+          setIp("0.0.0.0");
+        })
+        .then((data: any) => {
+          if (data !== undefined) {
+            setIp(data.ip);
+            setLocation(`${data.city}, ${data.country_name}`);
+            setItem('sessionIP', data.ip);
+            setItem('sessionLocation', `${data.city}, ${data.country_name}`);
+          }
+        });
+    } else {
+      setIp(sessionIP)
+      setLocation(sessionLocation)
+    }
+  } , []);
   useEffect(() => {
     if (
       // !whiteListIp.includes(ip) &&
