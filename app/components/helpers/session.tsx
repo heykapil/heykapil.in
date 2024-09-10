fb44b40e6033fe9b7bf42e7e910d72f00ea80f31
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2024-09-10 22:01:25 +0530

    .

--- app/components/helpers/session.tsx
+++ app/components/helpers/session.tsx
@@ -1,8 +1,8 @@
 import { cookies } from 'next/headers';
 import { decryptToken } from './paseto';
-import { redis } from './redis';
-import { Logout } from 'app/db/actions';
-import { redirect } from 'next/navigation';
+// import { redis } from './redis';
+// import { Logout } from 'app/db/actions';
+// import { redirect } from 'next/navigation';
 export async function Session() {
   let session: any;
   const profileToken = cookies().get('profileToken')?.value || '';
@@ -16,17 +16,18 @@ export async function Session() {
     session = null;
   }
   if (profileToken) {
-    const decodedToken = await decryptToken(profileToken);
-    const pool = await redis();
-    const redissessionId = await pool.get(decodedToken?.userid);
-    console.log(redissessionId);
-    if (redissessionId === sessionId) {
-      session = decodedToken;
-    } else {
-      session = null;
-      redirect('/signout?error=Session Expired');
-    }
-    await pool.quit();
+    session = await decryptToken(profileToken);
+    // const decodedToken = await decryptToken(profileToken);
+    // const pool = await redis();
+    // const redissessionId = await pool.get(decodedToken?.userid);
+    // console.log(redissessionId);
+    // if (redissessionId === sessionId) {
+    //   session = decodedToken;
+    // } else {
+    //   session = null;
+    //   redirect('/signout?error=Session Expired');
+    // }
+    // await pool.quit();
   } else {
     session = null;
   }
