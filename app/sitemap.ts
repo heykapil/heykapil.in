5833e36d60abcb67065a790675fce8ec968991d7
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2024-09-24 14:31:36 +0530

    last-mod-sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,22 +14,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const blogs = allPosts.map((post) => ({
     url: `https://kapil.app/musing/${post.slug}`,
     // title: post.metadata.title,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: post.metadata.updated?.split('T')[0],
   }));
   const snippets = allSnippets.map((snippet) => ({
     url: `https://kapil.app/snippet/${snippet.slug}`,
     title: snippet.metadata.title,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: snippet.metadata.updated?.split('T')[0],
   }));
   const quotes = allQuotes.map((quote) => ({
     url: `https://kapil.app/quotes/${quote.slug}`,
     // title: quote.metadata.title,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: quote.metadata.created?.split('T')[0],
   }));
   const extraPosts = allExtraPosts.map((post) => ({
     url: `https://kapil.app/extras/${post.slug}`,
     // title: post.metadata.title,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: post.metadata.created?.split('T')[0],
   }));
   const routes = [
     '',
