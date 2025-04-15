9c2ce0f23f69999287c9c8675084858421381633
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2025-04-15 15:15:12 +0530

    Update queries.tsx

--- app/db/queries.tsx
+++ app/db/queries.tsx
@@ -1,7 +1,12 @@
 import { queryBuilder } from './db';
-
+import { generateState } from 'app/components/helpers/random';
+import { signPasetoToken } from 'app/components/helpers/paseto';
 export const getViewsCount = async (slug: string) => {
-  const url = `https://kv.kapil.app/kv?key=pageviews,${slug}`;
+  const state = generateState();
+  const payload = { state };
+  const hash = await signPasetoToken({ payload });
+  const token = hash ? `v4.public.${hash}` : '';
+  const url = `https://kv.kapil.app/kv?key=pageviews,${slug}&state=${state}&token=${token}`;
   const response = await fetch(url);
   const data = await response.json();
   const count = data?.value?.value || 0;
