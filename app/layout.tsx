43bfa8546aa816c76f8eca3013fd8bbadb9c8a1e
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2025-11-22 14:08:20 +0530

    Add analytics script to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 import { Navbar } from './components/nav';
 import ToastProvider from 'app/components/toast-provider';
+import Script from 'next/script'
 export const metadata: Metadata = {
   metadataBase: new URL('https://kapil.app'),
   title: {
@@ -134,6 +135,7 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <Script defer src="https://analytics.kapil.app/script.js" data-website-id="eed82a85-bdd7-4035-8866-205c25b5fd51" />
         <ToastProvider>
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
             <Navbar />
